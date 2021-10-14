0f7119b52b8cb23354202bcca9c8a05e4e1b33d3
paulrohack <paulrock2005@gmail.com>
2021-10-14 12:31:45 +0530

    updated

--- QR_create.py
+++ QR_create.py
@@ -1,20 +1,25 @@
 import qrcode
-import os
+import os, tkinter, PIL
 
 data = input("Your Data or Link: ")
 img = qrcode.make(data)
+filename = "qrcode.png"
+img.save(filename)
+image = PIL.Image.open(filename)
+width, height = image.size
+root = tkinter.Tk()
+root.geometry(f'{width}x{height}')
+# root.maxsize()
+root.wm_title(f"QRCODE - {data}")
+canvas = tkinter.Canvas(root, width = width, height = height)      
+canvas.pack(side='left')      
+img = tkinter.PhotoImage(file=filename)      
+canvas.create_image(0,0, anchor='nw',image=img)  
+root.mainloop()
+image.close()
+os.system(f"del {filename}")
 
-save = input("Name of The QR-code: ")
 
-dirName = 'Qr_Images'
-try:
-    os.mkdir(dirName)
-    print("Directory " , dirName ,  " Created ")
-    img.save(f'Qr_Images/{save}.jpg')
-    print("QR saved")
- 
-except FileExistsError:
-    img.save(f'Qr_Images/{save}.jpg')
-    print("QR saved")
+
    
 
