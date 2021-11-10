f6b5d6961f6d8e82e0c9ad0b2ee33f27260e880c
paulrohack <paulrock2005@gmail.com>
2021-11-10 22:37:03 +0530

    pdated

--- CLI_Games/SpeedTypeTest.py
+++ CLI_Games/SpeedTypeTest.py
@@ -2,37 +2,46 @@ import time
 from random import choice
 import os
 
-words = ['about','act','actually','add','name','after','again','against','age','ago','air','all','also','always','among','another','answer','appear','are','area','as','ask','at',
-'you','back','ball','base','be','beauty','because','become','bed','been','before','began','begin','behind','best','better','between','big','bird','black','blue','boat',
-'body','book','both','bottom','box','boy','bring','brought','build','built','busy','call','came','can','car','care','carefully','carry','centre','certain','change',
-'check','child','children','city','class','clear','close','cold','colour','come','common','community','complete','contain','could','country','course','create','cried',
-'cross','cry','cut']
-
+words = []
+with open("CLI_Games\easy_words.txt", 'r') as f:
+    w = f.readlines()
+for i in (w):
+    words.append(i.replace('\n', '').lower())
 def mean(n):
     total_sum = sum(n)
     total_nos = len(n)
+    if total_nos == 0:
+        total_nos = 0.1
     return round((total_sum/total_nos), 2)
 time_taken = []
+os.system('cls')
 turns = int(input("How may Turns Would you want to try [recommended: 15 or 30 or 60]:  "))
 if turns < 10:
     print("Number of words are taken as 10, because of the very less word limit!")
     turns = 10
 
 words_made_wrong = []
-os.system('cls')
+e = 0
 print("When your ready hit ENTER!")
 if input() == "":
-    for i in range(turns):
+    print(" ")
+    os.system('cls')
+    while e < turns:
         word_given = choice(words)
-        print(word_given)
         start = time.time()
-        word =  input().lower().split()
+        word =  input(word_given + ": ").lower().split()
         end = time.time()
         Time = end-start
-        if word[0] != word_given:
+        if word == [] :
+            e -= 1
+        elif word[0] != word_given:
             words_made_wrong.append(word_given)
         else:
-            time_taken.append(Time)
-        os.system('cls')
-    print("WPS >>", mean(time_taken), f"\nThat means you took Average of {mean(time_taken)} seconds.")
-    print("Words that need More Practice", words_made_wrong)
+            time_taken.append(Time)            
+        e += 1
+    if len(words_made_wrong) > turns - 2 or mean(time_taken) == 0.0:
+        print("You better take this test Serious!")
+    else:
+        print("WPS: ", mean(time_taken), f" ==> That means you took Average of {mean(time_taken)} seconds.")    
+        print("Words that need More Practice", words_made_wrong)
+
